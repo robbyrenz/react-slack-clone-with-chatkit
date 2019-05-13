ddb92fdf80915afcaef68d628bc10aa2ccf2c56f
Robby Renz <nxgrenz@gmail.com>
2019-05-13 16:09:46 -0500

    install dep chatkit-server, and update server.js

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+	instanceLocator: '',
+	key: ''
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+	const { username } = req.body
+	chatkit.
+		createUser({
+			id: username,
+			name: username
+		})
+		.then(() => res.sendStatus(201))
+		.catch(error => {
+			if (error.error === 'services/chatkit/user_already_exists') {
+				res.sendStatus(200)
+			} else {
+				res.status(error.status).json(error)
+			}
+		})
+})
+
+app.post('/authenticate', (req, res) => {
+	const authData = chatkit.authenticate({ userId: req.query.user_id })
+	res.status(authData.status).send(authData.body)
+})
 
 const PORT = 3001
 app.listen(PORT, err => {
